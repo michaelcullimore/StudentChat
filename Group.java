2585711db02918906f4ca1d23d5583573e2b19b4
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-03-30 21:46:17 -0600

    Well, I don't know what happened and why I can't get it to work on my other repo, but this will work. Made "Group" added some changes.

--- Group.java
+++ Group.java
@@ -4,126 +4,39 @@
 
 package cs3230;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
 public class Group {
-    // Represents class of students
-    private ArrayList<Student> SortedClass;
-    private Class classroom = new Class();
-
-    // the conversations list
-    private ArrayList<String> convo;
-
-    // Read as Group to Students
-    private HashMap<String, HashSet> GroupToStudents;
-    private HashMap<Student, String> StudentToGroup;
-    private HashMap<String, ArrayList> GroupToMessages;
-
-    // Group size
-    private int size;
-
-    // initializes things for the group
-    public Group() {
-	SortedClass = new ArrayList<>();
-	GroupToStudents = new HashMap<>();
-	StudentToGroup = new HashMap<>();
-	GroupToMessages = new HashMap<>();
-	convo = new ArrayList<>();
-
-	size = 0;
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
 
     }
 
-    // Add 2 students to the chat group
-    public void addStudentToGroup(Student Student1, Student Student2) {
-	HashSet<Student> Groupof2Students = new HashSet<>();
+    Student sA;
+    Student sB;
 
-	Groupof2Students.add(Student1);
-	Groupof2Students.add(Student2);
-	size++;
-
-	GroupToStudents.put("Group" + size, Groupof2Students);
-	StudentToGroup.put(Student1, "Group" + size);
-	StudentToGroup.put(Student2, "Group" + size);
-
-    }
-
-    // chat method between 2 people
-    public void Chat(Student s, String message) {
-	String group = findStudentGroup(s.firstName);
-
-	if (s.thingsStudentHasSaid.contains(s.firstName + " " + s.lastName + " : " + message)) {
-	    return;
-
-	} else {
-	    convo.add(s.firstName + " " + s.lastName + " : " + message);
-	    GroupToMessages.put(group, convo);
-	    s.thingsStudentHasSaid.add(s.firstName + " " + s.lastName + " : " + message);
+    public Group(Student student1, Student student2) {
+	int compareFirstName = student1.getFirstName().compareTo(student2.getFirstName());
+	if (student1.equals(student2)) {
+	    throw new IllegalArgumentException("Error. Names must be unique.");
 	}
-
-    }
-
-    // creates the student class
-    public List<Student> CreateStudents() {
-	Collections.sort(classroom.studentArrayList);
-
-	for (String s : classroom.studentArrayList) {
-	    String[] parts = s.split(" ");
-	    Student asdf = new Student(parts[0], parts[1], 0);
-	    SortedClass.add(asdf);
-
-	}
-
-	return SortedClass;
-
-    }
-
-    // displays the convo
-    public ArrayList<String> FindConversationsOfStudent(String firstName) {
-	String group = findStudentGroup(firstName);
-
-	ArrayList<String> conversation = GroupToMessages.get(group);
-	return conversation;
-
-    }
-
-    // "Finds" a student based on the first name
-    public Student findStudent(String first) {
-
-	for (Student item : SortedClass) {
-
-	    if (item.firstName.equals(first)) {
-		return item;
+	if (compareFirstName < 0) {
+	    this.sA = student1;
+	    this.sB = student2;
+	} else if (compareFirstName > 0) {
+	    this.sA = student2;
+	    this.sB = student1;
+	} else if (compareFirstName == 0) {
+	    int compareLastName = student1.getLastName().compareTo(student2.getLastName());
+	    if (student1.equals(student2)) {
+		throw new IllegalArgumentException("Error. Names must be unique");
 	    }
-
+	    if (compareLastName < 0) {
+		this.sA = student1;
+		this.sB = student2;
+	    } else if (compareLastName > 0) {
+		this.sA = student2;
+		this.sB = student1;
+	    }
 	}
-
-	return null;
-
-    }
-
-    // finds the students by their name
-    private String findStudentGroup(String name) {
-	Student s = findStudent(name);
-	String group = StudentToGroup.get(s);
-	return group;
-    }
-
-    // generates the groups for the people in the class
-    public int GenerateGroups(List<Student> allStudents) {
-	int count = 0;
-
-	for (int i = 0; i < allStudents.size() / 2; i++) {
-	    addStudentToGroup(allStudents.get(count), allStudents.get(count + 1));
-	    count = count + 2;
-	}
-
-	return 0;
-
     }
 
 }
--- Student.java
+++ Student.java
@@ -4,36 +4,47 @@
 
 package cs3230;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
-    String firstName;
-    String lastName;
+    public static void main(String[] args) {
+	// make a test conversation, create a student
+	String[] testconvo = { "test", "hi", "Guten Morgen!", "Hey", "g2g" };
+	Student student01 = new Student("Michael", "Cullimore", 97.5, testconvo);
 
-    // track the messages
-    HashSet<String> thingsStudentHasSaid;
+	// test
+	System.out.println(student01.toString());
+	System.out.println(student01.conversations.get(0));
+    }
 
-    public Student(String first, String last, int score1) {
-	firstName = first;
-	lastName = last;
-	thingsStudentHasSaid = new HashSet<>();
+    private String firstName;
+    private String lastName;
 
+    private double score;
+
+    ArrayList<String> conversations;
+
+    // create a student
+    public Student(String firstN, String lastN, double _score, String[] convos) {
+	this.firstName = firstN;
+	this.lastName = lastN;
+	this.score = _score;
+	this.conversations = new ArrayList<>(Arrays.asList(convos));
     }
 
     public String getFirstName() {
+	// TODO Auto-generated method stub
 	return firstName;
     }
 
     public String getLastName() {
+	// TODO Auto-generated method stub
 	return lastName;
     }
 
-    public void setFirstName(String firstName) {
-	this.firstName = firstName;
+    public double getScore() {
+	// TODO Auto-generated method stub
+	return score;
     }
-
-    public void setLastName(String lastName) {
-	this.lastName = lastName;
-    }
-
 }
