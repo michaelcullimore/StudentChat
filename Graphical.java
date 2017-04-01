3f394f4610f508c94dfe0e56e716c2ea909b385c
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-04-01 13:48:06 -0600

    Starting the Graphical Chat section

---
+++ Graphical.java
@@ -0,0 +1,21 @@
+package cs3230;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class Graphical extends JFrame {
+    private static final long serialVersionUID = -7365155579638644639L;
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+    JPanel myPanel01;
+    JTextField textField01;
+    JLabel label01;
+    JScrollPane scrollBar01;
+}
