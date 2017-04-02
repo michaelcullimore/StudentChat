bd5f0e3ac3f96230ca17e766f37ca5df9900e670
Michael Cullimore <michaelcullimore@mail.weber.edu>
2017-04-02 12:31:14 -0600

    no message

---
+++ Server.java
@@ -0,0 +1,13 @@
+package cs3230;
+
+public class Server {
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+    public Server() {
+	// TODO Auto-generated constructor stub
+    }
+
+}
